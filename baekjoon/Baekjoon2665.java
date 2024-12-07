a786ac90c4b897e3e20158d4315bfd79dda68da9
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-12-07 16:29:05 +0900

    2665 메모리초과 해결

--- baekjoon/Baekjoon2665.java
+++ baekjoon/Baekjoon2665.java
@@ -1,65 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+class Drill {
+	int y;
+	int x;
+	int drill;
+	Drill(int y,int x,int drill){
+		this.y=y;
+		this.x=x;
+		this.drill=drill;
+	}
+}
 
 public class Baekjoon2665 {
-    static int map[][];
-    static int dx[] = {0,1,0,-1};
-    static int dy[] = {1,0,-1,0};
-    static int dist[][];
-    static int N;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        dist = new int[N][N];
+	static char map[][];
+	static int xx[]= {-1,1,0,0};
+	static int yy[]= {0,0,-1,1};
+    static int min[][];
+	public static void main(String[] args) throws IOException {
 
-        for(int i=0;i<N;i++){
-            String str = br.readLine();
-            for(int j=0;j<N;j++){
-                map[i][j] = str.charAt(j)-'0';
-                dist[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        bfs();
+		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+
+		int n=Integer.parseInt(br.readLine());
+		map=new char[n][n];
+		min=new int[n][n];
+		for(int i=0;i<n;i++) {
+			String word=br.readLine();
+			for(int j=0;j<n;j++) {
+				map[i]=word.toCharArray();
+			}
+		}
+		
+		bfs();
+		System.out.println(min[n-1][n-1]);
+		
+	}
+	public static void bfs() {
+		Queue<Drill> queue=new LinkedList<>();
+	
+		for(int i=0;i<min.length;i++)
+			Arrays.fill(min[i],1000000);
+		//1은 흰방 0은 검은 방
+		if(map[0][0]=='0') {
+			queue.add(new Drill(0,0,1));
+			min[0][0]=1;
+		}
+		else {
+			queue.add(new Drill(0,0,0));
+			min[0][0]=0;
+		}
+		
+		while(!queue.isEmpty()) {
+			Drill temp=queue.poll();
+			int prevY=temp.y;
+			int prevX=temp.x;
+			int drill=temp.drill;
+			for(int i=0;i<4;i++) {
+				int nextY=prevY+yy[i];
+				int nextX=prevX+xx[i];
+				if(nextY<0||nextX<0||nextY>=map.length||nextX>=map.length)
+					continue;
+				if(map[nextY][nextX]=='0'&&drill+1>=min[nextY][nextX])
+					continue;
+				if(map[nextY][nextX]=='1'&&drill>=min[nextY][nextX])
+					continue;
+				
+				
+				if(map[nextY][nextX]=='1') {
+					queue.add(new Drill(nextY,nextX,drill));
+					min[nextY][nextX]=drill;
+				}
+				else {
+					queue.add(new Drill(nextY,nextX,drill+1));
+					min[nextY][nextX]=drill+1;
+				}
+				
+				
+			}
+		}
 
 
-        System.out.println(dist[N-1][N-1]);	//배열은 0부터 시작이므로 
-    }
-    public static void bfs(){
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0,0));   //시작점
-        dist[0][0] = 0;
-
-        while(!q.isEmpty()){
-            Node now = q.poll();
-
-            for(int i=0;i<4;i++){   //상하좌우 탐색
-                int nextX = now.x+dx[i];
-                int nextY = now.y+dy[i];
-
-                if(nextX >=0 && nextY >= 0 && nextX < N && nextY < N){
-                    if(dist[nextX][nextY] > dist[now.x][now.y]){
-                        if(map[nextX][nextY] == 1) dist[nextX][nextY] = dist[now.x][now.y];
-                        else dist[nextX][nextY] = dist[now.x][now.y]+1; //이동
-                        q.add(new Node(nextX,nextY));
-                    }
-                }
-            }
-        }
-    }
-
-
-    public static class Node{
-        int x;
-        int y;
-        public Node(int x,int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+		
+	}
 }
